7d09c901449c47549e999f6fc2851adde0b9a12d
unknown <demchakromka@gmail.com>
2025-02-17 17:51:28 +0300

    ошибка emmit trigger

--- src/components/model.ts
+++ src/components/model.ts
@@ -1,10 +1,17 @@
 import { IProductItem } from "../types";
+import { IEvents } from "./base/events";
 
 export class ProductModel {
     protected products:  IProductItem[] = [];
+    private events: IEvents;
+
+    constructor(events: IEvents) {
+        this.events = events;
+    }
 
     setProducts(products: IProductItem[]) {
         this.products = products
+        this.events.emit('products-loaded', this.products)
     }
 
     getProducts(): IProductItem[] {
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,46 @@
 import { ProductApi } from './components/apiProducts';
 import { ProductModel } from './components/model';
 import './scss/styles.scss';
-import { Api } from './components/base/api';
+// import { Api } from './components/base/api';
 import { API_URL } from './utils/constants';
+import { ProductView } from './components/view';
+import { cloneTemplate } from './utils/utils';
+import { EventEmitter } from 'events';
 
-const cardTemplate = document.querySelector('#success');
-console.log(cardTemplate)
+
+const templateOrderSuccess = document.querySelector('#success') as HTMLTemplateElement;
+const templateCard = document.querySelector('#card-catalog') as HTMLTemplateElement;
+const templateCardPreview = document.querySelector('#card-preview') as HTMLTemplateElement;
+const templateCardBasket = document.querySelector('#card-basket') as HTMLTemplateElement;
+const templateBasket = document.querySelector('#basket') as HTMLTemplateElement;
+const templateOrder = document.querySelector('#order') as HTMLTemplateElement; 
+const templateContacts = document.querySelector('#contacts') as HTMLTemplateElement;   
+
+
+// const cardTemplate = document.querySelector('#success');
+// console.log(cardTemplate)
+console.log(templateCard)
 
 const btnProfile = document.querySelector('.modal_active');
-
-console.log(btnProfile)
 btnProfile.classList.remove('modal_active');
 
-const productModel = new ProductModel();
+// console.log(btnProfile)
+const gallery = document.querySelector('.gallery')
+const events = new EventEmitter();
+const productModel = new ProductModel(events);
+const products = new ProductView(cloneTemplate(templateCard))
+
 
 const api = new ProductApi(API_URL)
 api.getProducts().then((data)=>{
-    productModel.setProducts(data)
+    productModel.setProducts(data);
+    
     console.log(productModel);
 })
 .catch((err)=>{
     console.error(err);
 });
+
 
 
 
