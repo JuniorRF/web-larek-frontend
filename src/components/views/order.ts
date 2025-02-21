a469b2dadc557ae29b71c5943c4cf0d3b6965c92
unknown <demchakromka@gmail.com>
2025-02-21 20:19:58 +0300

    + valid addres

--- src/components/views/order.ts
+++ src/components/views/order.ts
@@ -7,6 +7,7 @@ export class OrderModal extends Modal {
     protected buttonSubmit: HTMLButtonElement;
     protected payment: HTMLButtonElement[];
     protected formOrder: HTMLFormElement;
+    protected addressError: HTMLElement;
 
     show(template: HTMLTemplateElement): void {
         this.setContent(template);
@@ -14,8 +15,10 @@ export class OrderModal extends Modal {
         this.buttonSubmit = ensureElement<HTMLButtonElement>('.order__button', this.content);
         this.payment = ensureAllElements<HTMLButtonElement>('.button_alt', this.content);
         this.formOrder = ensureElement<HTMLFormElement>('form[name="order"]', this.content);
+        this.addressError = ensureElement<HTMLElement>('.form__errors_address', this.content);
 
         this.setPaymentMethod('card');
+        
 
         this.payment.forEach((item) => {
             item.addEventListener('click', () => {
@@ -24,10 +27,10 @@ export class OrderModal extends Modal {
         });
 
         this.formOrder.addEventListener('input', (event: Event) => {
-            // const target = event.target as HTMLInputElement;
-            // const field = target.name;
-            // const value = target.value;
-            // this.events.emit('order:changeAddress', { field, value });
+            const input = event.target as HTMLInputElement;
+            if (input.name === 'address') {
+                this.validateAddress(input);
+            }
             this.buttonSubmit.disabled = !this.isFormValid();
         });
       
@@ -44,6 +47,12 @@ export class OrderModal extends Modal {
         this.open();
     }
 
+    protected validateAddress(input: HTMLInputElement): void {
+        const isValid = input.value.length > 3;
+        this.addressError.textContent = !input.value ? 'Необходимо указать адрес' : 
+                                    !isValid ? 'Адрес должен быть не менее 3 символов' : '';
+    }
+
     protected setPaymentMethod(method: string): void {
         this.payment.forEach(item => {
             item.classList.toggle('button_alt-active', item.name === method);
@@ -52,7 +61,7 @@ export class OrderModal extends Modal {
 
     protected isFormValid(): boolean {
         const addressInput = this.formOrder.querySelector('[name="address"]') as HTMLInputElement;
-        return addressInput?.value.length > 0;
+        return addressInput?.value.length > 3;
     }
 }
 
